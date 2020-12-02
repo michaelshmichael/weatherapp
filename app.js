471dd417b25a2835ade11868016eb3fa905906d1
logeybogey <michaeldcjohnson@gmail.com>
2020-12-02 13:32:59 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+async function getLocationData () {
+    //get the data for the weather at the user inputted location
+}
+
+async function returnLocationData () {
+    //return weather data for that location
+}
